08707d8bf25edde4b67e41439c9f6746f464345b
hq <hq_cml@163.com>
2016-05-09 08:43:03 +0800

    基本的函数

--- cases/http/http.c
+++ cases/http/http.c
@@ -6,9 +6,9 @@
  * |  `--'  | |  |\   | |  | |  `----.|  `--'  | |  |\  \-.|  |\   | 
  *  \______/  |__| \__| |__|  \______| \______/  | _| `.__||__| \__| 
  *
- *    Filename :  tcp.c
+ *    Filename :  http.c
  * 
- * Description :  Unicorn框架tcp压测客户端
+ * Description :  基于Unicorn框架的HTTP压测客户端
  *                实现unc_plugin.h中的函数，即可将用户逻辑嵌入unicorn框架
  * 
  *     Version :  1.0.0 
@@ -16,4 +16,69 @@
  *      Author :  HQ 
  *
  **/
+
+#include "unc_core.h"
+
+/**
+ * 功能: 初始化回调
+ * 参数: @conf, @args
+ * 说明: 
+ *       1. 可选函数
+ * 返回:成功:0; 失败:-x
+ **/
+int unc_handle_init(void *conf, void *args) 
+{
+    conf_t *p_conf =(conf_t *) conf;
+    p_conf->title = "TCP PRESSURE TEST";
+    printf("************* WELCOME TO UNICORN TCP PRESSURE TEST ***************\n\n");
+    return UNC_OK;
+}
+
+/**
+ * 功能: 结束回调
+ * 参数: @conf, @args
+ * 说明: 
+ *       1. 可选函数
+ **/
+int unc_handle_finish(void *conf, void *args) 
+{
+    conf_t *p_conf =(conf_t *) conf;
+    if(p_conf->response.is_get)
+    {
+        printf("====== THE SERVER RESPONSE ======\n");
+        printf("%s\n", p_conf->response.res_body->buf);
+    }
+    printf("**************** THANK YOU FOR USE UNICORN. BYE! ****************\n");
+
+    return UNC_OK;
+}
+
+/**
+ * 功能: 请求前回调
+ * 参数: @conf, @args
+ * 说明: 
+ *       1. 可选函数
+ * 返回:成功:0; 失败:-x
+ **/
+int unc_request_pre(void *conf, void *args) 
+{
+    printf("*************************** TEST START ***************************\n");
+    return UNC_OK;
+}
+
+/**
+ * 功能: 请求后回调
+ * 参数: @conf, @args
+ * 说明: 
+ *       1. 可选函数
+ * 返回:成功:0; 失败:-x
+ **/
+int unc_request_post(void *conf, void *args) 
+{
+    printf("*************************** TEST END ***************************\n\n\n\n");
+    return UNC_OK;
+}
+
+
+
 
