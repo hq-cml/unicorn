aeeb4361b34b010d6e118736075c552c9ac6e079
hq <hq_cml@163.com>
2016-05-11 08:50:27 +0800

    http客户端兼容read file

--- cases/http/http.c
+++ cases/http/http.c
@@ -19,6 +19,48 @@
 
 #include "unc_core.h"
 
+
+/**
+ * 功能: 生成Tcp请求body
+ * 参数: @conf, @args
+ * 说明: 
+ *       1. 可选函数
+ * 返回:成功:0; 失败:-x
+ **/
+int unc_generate_request(void *conf, void *args) 
+{
+    conf_t *p_conf =(conf_t *) conf;
+    if(!p_conf->request_body)
+    {
+        p_conf->request_body = unc_str_new("GET /hello.php 1.0\r\n\r\n");
+    }
+
+    return UNC_OK;
+}
+
+/**
+ * 功能: 判断当前的response内容，是否是一个完整的resoponse
+ * 参数: @conf, @client, @args
+ * 说明: 
+ *       1. 必选函数
+ * 返回:成功:0; 失败:-x
+ **/
+int unc_check_full_response(void *conf, void *client, void *args) 
+{
+    client_t *p_client =(client_t *) client;
+    return UNC_NEEDMORE;
+
+    char * pos = strstr(p_client->recvbuf->buf, "world");
+    if(pos != NULL)
+    {
+        return UNC_OK;
+    }
+    else
+    {
+        return UNC_NEEDMORE;
+    }
+}
+
 /**
  * 功能: 初始化回调
  * 参数: @conf, @args
@@ -29,8 +71,8 @@
 int unc_handle_init(void *conf, void *args) 
 {
     conf_t *p_conf =(conf_t *) conf;
-    p_conf->title = "TCP PRESSURE TEST";
-    printf("************* WELCOME TO UNICORN TCP PRESSURE TEST ***************\n\n");
+    p_conf->title = "HTTP PRESSURE TEST";
+    printf("************* WELCOME TO UNICORN HTTP PRESSURE TEST ***************\n\n");
     return UNC_OK;
 }
 
@@ -79,44 +121,3 @@ int unc_request_post(void *conf, void *args)
     return UNC_OK;
 }
 
-/**
- * 功能: 生成Tcp请求body
- * 参数: @conf, @args
- * 说明: 
- *       1. 可选函数
- * 返回:成功:0; 失败:-x
- **/
-int unc_generate_request(void *conf, void *args) 
-{
-    conf_t *p_conf =(conf_t *) conf;
-    //TODO read from a file
-    /*
-    GET /www/hello.php
-    Connection:Keep-Alive
-    */
-    //p_conf->request_body = unc_str_new("GET /www/info.php 1.0\r\nConnection:Keep-Alive\r\n\r\n");
-    p_conf->request_body = unc_str_new("GET /www/info.php 1.0\r\nConnection:Keep-Alive\r\nAccept:text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\r\nAccept-Encoding:gzip, deflate, sdch\r\nAccept-Language:zh-CN,zh;q=0.8,en;q=0.6\r\nCache-Control:max-age=0\r\nConnection:keep-alive\r\nHost:127.0.0.1:8081\r\nUpgrade-Insecure-Requests:1\r\nUser-Agent:Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.94 Safari/537.36\r\n\r\n");
-    return UNC_OK;
-}
-
-/**
- * 功能: 判断当前的response内容，是否是一个完整的resoponse
- * 参数: @conf, @client, @args
- * 说明: 
- *       1. 必选函数
- * 返回:成功:0; 失败:-x
- **/
-int unc_check_full_response(void *conf, void *client, void *args) 
-{
-    client_t *p_client =(client_t *) client;
-
-    char * pos = strstr(p_client->recvbuf->buf, "world");
-    if(pos != NULL)
-    {
-        return UNC_OK;
-    }
-    else
-    {
-        return UNC_NEEDMORE;
-    }
-}
