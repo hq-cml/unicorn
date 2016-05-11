51bef82fa3e71a63e8b3e81781e3cd9ae65c019e
hq <hq_cml@163.com>
2016-05-11 08:48:11 +0800

    TCP客户端兼容read file

--- cases/tcp/tcp.c
+++ cases/tcp/tcp.c
@@ -19,6 +19,48 @@
 
 #include "unc_core.h"
 
+
+/**
+ * 功能: 生成Tcp请求body
+ * 参数: @conf, @args
+ * 说明: 
+ *       1. 可选函数
+ * 返回:成功:0; 失败:-x
+ **/
+int unc_generate_request(void *conf, void *args) 
+{
+    conf_t *p_conf =(conf_t *) conf;
+    if(!p_conf->request_body)
+    {
+        p_conf->request_body = unc_str_new("HaHa~");
+    }
+    return UNC_OK;
+}
+
+/**
+ * 功能: 判断当前的response内容，是否是一个完整的resoponse
+ * 参数: @conf, @client, @args
+ * 说明: 
+ *       1. 必选函数
+ * 返回:成功:0; 失败:-x
+ **/
+int unc_check_full_response(void *conf, void *client, void *args) 
+{
+    client_t *p_client =(client_t *) client;
+
+    //对于TCP回显示压测程序: 
+    //当读取到的数据，和写出去的数据相等时，算是完成了一次请求
+    if(p_client->read == p_client->sendbuf->len 
+       && 0==memcmp(p_client->recvbuf->buf, p_client->sendbuf->buf, p_client->sendbuf->len))
+    {
+        return UNC_OK;
+    }
+    else
+    {
+        return UNC_NEEDMORE;
+    }
+}
+
 /**
  * 功能: 初始化回调
  * 参数: @conf, @args
@@ -78,45 +120,6 @@ int unc_request_post(void *conf, void *args)
     printf("*************************** TEST END ***************************\n\n\n\n");
     return UNC_OK;
 }
-
-/**
- * 功能: 生成Tcp请求body
- * 参数: @conf, @args
- * 说明: 
- *       1. 可选函数
- * 返回:成功:0; 失败:-x
- **/
-int unc_generate_request(void *conf, void *args) 
-{
-    conf_t *p_conf =(conf_t *) conf;
-    p_conf->request_body = unc_str_new("HaHa~");
-    return UNC_OK;
-}
-
-/**
- * 功能: 判断当前的response内容，是否是一个完整的resoponse
- * 参数: @conf, @client, @args
- * 说明: 
- *       1. 必选函数
- * 返回:成功:0; 失败:-x
- **/
-int unc_check_full_response(void *conf, void *client, void *args) 
-{
-    client_t *p_client =(client_t *) client;
-
-    //对于TCP回显示压测程序: 
-    //当读取到的数据，和写出去的数据相等时，算是完成了一次请求
-    if(p_client->read == p_client->sendbuf->len 
-       && 0==memcmp(p_client->recvbuf->buf, p_client->sendbuf->buf, p_client->sendbuf->len))
-    {
-        return UNC_OK;
-    }
-    else
-    {
-        return UNC_NEEDMORE;
-    }
-}
-
 
 
 
