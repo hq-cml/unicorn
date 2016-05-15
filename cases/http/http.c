0c640b2d21ae871757ad1ddcb957ffefe3364c1c
hq <hq_cml@163.com>
2016-05-15 09:23:25 +0800

    调优http,规避EPIPE

--- cases/http/http.c
+++ cases/http/http.c
@@ -57,11 +57,6 @@ int unc_generate_request(void *conf, void *args)
  *     HTTP HEADER\r\n
  *     \r\n
  *     HTTP BODY
- *
- * BODY长度判断方法:
- * 1. 如果header中包含chunked，则用chunked模式确定body长度(优先级高于Content-Length)
- * 2. 如果header中包含Content-Length，则用Content-Length作为body长度
- * 3. 如果都没有，则利用服务器close连接来进行判断body长度
  **/
 int unc_check_full_response(void *conf, void *client, void *args) 
 {
@@ -145,6 +140,156 @@ int unc_handle_server_close(void *conf, void *client, void *args)
     return UNC_OK;
 }
 
+
+
+/*
+ * 功能: 计算body长度
+ * 参数: @header_start, @header_length
+ * 说明: 
+ *       0. Connection:close优先级最高，可以规避unicorn遇到EPIPE的问题
+ *       1. 查找Connection:close，如果找到则表示务端断开连接来判断长度，返回-2
+ *       2. 先找Transfer-Encoding: chunked，如果找到，则返回-1
+ *       3. 查找Content-Length，如果找到返回长度
+ *       4. 都没找到，报错
+ * 返回:
+ *       HTTP_BODY_CHUNKED   : 表示Transfer-Encoding: chunked
+ *       整数                : 表示Content-Length
+ *       HTTP_BODY_CLOSE     : 表示服务器会断开连接
+ *       HTTP_BODY_ERR       : 未知错误
+ */
+static int cal_body_length(char *header_start, int header_length)
+{
+    unc_str_t *body;
+    char *ptr;
+    int len;
+    body = unc_str_newlen(header_start, header_length);  
+    //TODO 对比g_http_response_header，看看是否会发生变化
+
+    
+    if((strcasestr(body->buf, "Connection: close"))
+        || (strcasestr(body->buf, "Connection:close"))) 
+    {
+        //Connection: Close
+        unc_str_free(body);
+        return HTTP_BODY_CLOSE;
+    }
+    //TODO 查找chunked else if(chunked)
+    else if((ptr = strcasestr(body->buf, "Content-Length:"))) 
+    {
+        //Content-Length:
+        len = strtol(ptr + strlen("Content-Length:"), NULL, 10);
+        if (len  == 0) 
+        {
+            fprintf(stderr, "Invalid http protocol");
+            exit(1);
+        }
+        unc_str_free(body);
+        return len;
+    }
+    else
+    {
+        unc_str_free(body);
+        return HTTP_BODY_ERR;
+    }
+}
+
+/*
+ * 功能: 根据body_length，处理body
+ * 参数: @body_start, @body_length
+ * 说明: 
+ *
+ * 返回:
+ *      UNC_OK         : 符合一个完整的包
+ *      UNC_NEEDMORE   : 包长不够，需要框架继续read
+ *      UNC_ERR        : 出现未知错误
+ */
+static int handle_body(const char *body_start, int body_length)
+{
+    if(body_length== HTTP_BODY_CLOSE)
+    {
+        //Connection: Close
+        return UNC_NEEDMORE;
+    }
+    else if(body_length == HTTP_BODY_CHUNKED)
+    {
+        //Connection: Keep-Alive
+        //Transfer-Encoding: chunked
+        //TODO 处理chunked
+        printf("Something Wrong1");
+        exit(1);
+    }
+    else if(body_length > 0)
+    {
+        //Connection: Keep-Alive
+        //Content-Length: xxx
+        return handle_body_content_length(body_start, body_length);
+    }
+    else
+    {
+        //TODO return err
+        printf("Something Wrong3");
+        exit(1);
+    }
+}
+
+/*
+ * 功能: 处理 Content-Length
+ * 参数: @body_start, @body_length
+ * 说明: 
+ *
+ * 返回:
+ *      UNC_OK         : 符合一个完整的包
+ *      UNC_NEEDMORE   : 包长不够，需要框架继续read
+ *      UNC_ERR        : 出现未知错误
+ */
+static int handle_body_content_length(const char *body_start, int body_length)
+{
+    int len = strlen(body_start);
+    if(len == body_length)
+    {
+        if(!g_http_response_body)
+        {
+             g_http_response_body = unc_str_newlen(body_start, body_length);    
+        }
+        return UNC_OK;
+    }
+    else if(len > body_length)
+    {
+        fprintf(stderr, "Get Too More. Recvbuf len:%d, Expected body len:%d\n", len, body_length);
+        return UNC_ERR;
+    }
+    else
+    {
+        return UNC_NEEDMORE;
+    }    
+}
+
+/**
+ * 功能: 请求前回调
+ * 参数: @conf, @args
+ * 说明: 
+ *       1. 可选函数
+ * 返回:成功:0; 失败:-x
+ **/
+int unc_request_pre(void *conf, void *args) 
+{
+    fprintf(stdout, "************************** TEST START ****************************\n");
+    return UNC_OK;
+}
+
+/**
+ * 功能: 请求后回调
+ * 参数: @conf, @args
+ * 说明: 
+ *       1. 可选函数
+ * 返回:成功:0; 失败:-x
+ **/
+int unc_request_post(void *conf, void *args) 
+{
+    fprintf(stdout, "*************************** TEST END *****************************\n\n\n");
+    return UNC_OK;
+}
+
 /**
  * 功能: 初始化回调
  * 参数: @conf, @args
@@ -189,7 +334,7 @@ int unc_handle_finish(void *conf, void *args)
     if(g_http_response_body)
     {
         fprintf(stdout, "========= THE RESPONSE BODY ( Length:%5d ) =========\n", g_http_response_body->len);
-        fprintf(stdout, "%s\n", g_http_response_body->buf);
+        //fprintf(stdout, "%s\n", g_http_response_body->buf);
     }
     
     if(p_conf->response.is_get)
@@ -201,148 +346,5 @@ int unc_handle_finish(void *conf, void *args)
     return UNC_OK;
 }
 
-/**
- * 功能: 请求前回调
- * 参数: @conf, @args
- * 说明: 
- *       1. 可选函数
- * 返回:成功:0; 失败:-x
- **/
-int unc_request_pre(void *conf, void *args) 
-{
-    fprintf(stdout, "************************** TEST START ****************************\n");
-    return UNC_OK;
-}
-
-/**
- * 功能: 请求后回调
- * 参数: @conf, @args
- * 说明: 
- *       1. 可选函数
- * 返回:成功:0; 失败:-x
- **/
-int unc_request_post(void *conf, void *args) 
-{
-    fprintf(stdout, "*************************** TEST END *****************************\n\n\n");
-    return UNC_OK;
-}
-
-/*
- * 功能: 计算body长度
- * 参数: @header_start, @header_length
- * 说明: 
- *       1. 先找Transfer-Encoding: chunked，如果找到，则返回-1
- *       2. 查找Content-Length，如果找到返回长度
- *       3. 查找Connection:close，如果找到则表示务端断开连接来判断长度，返回-2
- *       4. 都没找到，报错
- * 返回:
- *       HTTP_BODY_CHUNKED   : 表示Transfer-Encoding: chunked
- *       整数                : 表示Content-Length
- *       HTTP_BODY_CLOSE     : 表示服务器会断开连接
- *       HTTP_BODY_ERR       : 未知错误
- */
-static int cal_body_length(char *header_start, int header_length)
-{
-    unc_str_t *body;
-    char *ptr;
-    int len;
-    body = unc_str_newlen(header_start, header_length);  
-    //TODO 对比g_http_response_header，看看是否会发生变化
-
-    //TODO 查找chunked
-    
-    if((ptr = strcasestr(body->buf, "Content-Length:"))) 
-    {
-        //Content-Length:
-        len = strtol(ptr + strlen("Content-Length:"), NULL, 10);
-        if (len  == 0) 
-        {
-            fprintf(stderr, "Invalid http protocol");
-            exit(1);
-        }
-        unc_str_free(body);
-        return len;
-    }
-    else if((strcasestr(body->buf, "Connection: close"))
-        || (strcasestr(body->buf, "Connection:close"))) 
-    {
-        //Connection: Close
-        unc_str_free(body);
-        return HTTP_BODY_CLOSE;
-    }
-    else
-    {
-        unc_str_free(body);
-        return HTTP_BODY_ERR;
-    }
-}
-
-/*
- * 功能: 根据body_length，处理body
- * 参数: @body_start, @body_length
- * 说明: 
- *
- * 返回:
- *      UNC_OK         : 符合一个完整的包
- *      UNC_NEEDMORE   : 包长不够，需要框架继续read
- *      UNC_ERR        : 出现未知错误
- */
-static int handle_body(const char *body_start, int body_length)
-{
-    if(body_length == HTTP_BODY_CHUNKED)
-    {
-        //TODO 处理chunked
-                printf("Something Wrong1");
-        exit(1);
-    }
-    else if(body_length > 0)
-    {
-        //Content-Length
-        return handle_body_content_length(body_start, body_length);
-    }
-    else if(body_length== HTTP_BODY_CLOSE)
-    {
-        //Connection: Close
-        return UNC_NEEDMORE;
-    }
-    else
-    {
-        //TODO return err
-        printf("Something Wrong3");
-        exit(1);
-    }
-}
-
-/*
- * 功能: 处理 Content-Length
- * 参数: @body_start, @body_length
- * 说明: 
- *
- * 返回:
- *      UNC_OK         : 符合一个完整的包
- *      UNC_NEEDMORE   : 包长不够，需要框架继续read
- *      UNC_ERR        : 出现未知错误
- */
-static int handle_body_content_length(const char *body_start, int body_length)
-{
-    int len = strlen(body_start);
-    if(len == body_length)
-    {
-        if(!g_http_response_body)
-        {
-             g_http_response_body = unc_str_newlen(body_start, body_length);    
-        }
-        return UNC_OK;
-    }
-    else if(len > body_length)
-    {
-        fprintf(stderr, "Get Too More. Recvbuf len:%d, Expected body len:%d\n", len, body_length);
-        return UNC_ERR;
-    }
-    else
-    {
-        return UNC_NEEDMORE;
-    }    
-}
 
 
