e3b8e1b3d1530c91aa6bb7267bf7b1f81e669f70
hq <hq_cml@163.com>
2016-05-22 11:28:13 +0800

    增加chunked.c专门处理chunked逻辑

---
+++ cases/http/chunked.c
@@ -0,0 +1,46 @@
+/**
+ *  __    __   __   __   __    ______   ______   .______    __   __  
+ * |  |  |  | |  \ |  | |  |  /      | /  __  \  |   _  \  |  \ |  | 
+ * |  |  |  | |   \|  | |  | |  ,----'|  |  |  | |  |_)  | |   \|  | 
+ * |  |  |  | |  . `  | |  | |  |     |  |  |  | |      /  |  . `  | 
+ * |  `--'  | |  |\   | |  | |  `----.|  `--'  | |  |\  \-.|  |\   | 
+ *  \______/  |__| \__| |__|  \______| \______/  | _| `.__||__| \__| 
+ *
+ *    Filename :  http.c
+ * 
+ * Description :  基于Unicorn框架的HTTP压测客户端
+ *                负责HTTP的chunked相关逻辑实现
+ * 
+ *     Version :  1.0.0 
+ * 
+ *      Author :  HQ 
+ *
+ **/
+
+#include "http.h"
+
+int HexStr2Dec(const char *hex, int len)
+{
+    int dec = 0;
+    int tmp = 0;
+    int i;
+    printf("len:%d\n", len);
+    for(i=0; i< len; i++)
+    {printf("%c\n", hex[i]);
+        if(hex[i]>='a' && hex[i]<='f'){
+            tmp=hex[i] - 'a' + 10;
+        }else if(hex[i]>='A' && hex[i]<='F'){
+            tmp=hex[i] - 'A' + 10;
+        }else if(hex[i]>='0' && hex[i]<='9'){
+            tmp=hex[i] - '0';
+        }else{
+            printf("%c\n", hex[i]);
+            return UNC_ERR;
+        }
+        dec = dec*16+tmp;
+    }
+    return dec;
+}
+
+
+
